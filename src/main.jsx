664d9d5418011ba9e61103d9a53b926ad1f31f2b
qasimfida <qasimfida0054@gmail.com>
2024-05-30 07:37:54 +0500

    removed strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,36 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.scss'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
 
-import {
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query';
-import Search from './screens/search.jsx';
-import { ModalProvider } from './components/modal';
-import { useModal } from './contexts/Modal';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Search from "./screens/search.jsx";
+import { ModalProvider } from "./components/modal";
+import { useModal } from "./contexts/Modal";
 
 // Create a client
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 const Button = () => {
-  const {openModal} = useModal()
+  const { openModal } = useModal();
   return (
-    <button id='searchLibBtn' onClick={() => openModal(true)}>HEllo</button>
-  )
-}
+    <button id="searchLibBtn" onClick={() => openModal(true)}>
+      HEllo
+    </button>
+  );
+};
 
-  ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-      <QueryClientProvider client={queryClient}>
-          <ModalProvider>
-            <Button />
-            <Search />
-          </ModalProvider>
-      </QueryClientProvider>
-    </React.StrictMode>,
-  )
-
-
-
-
-
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <div>
+    <QueryClientProvider client={queryClient}>
+      <ModalProvider>
+        <>
+          <Button />
+          <Search />
+        </>
+      </ModalProvider>
+    </QueryClientProvider>
+  </div>
+);
